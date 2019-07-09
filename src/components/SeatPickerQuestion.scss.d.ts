285843437621826c10bde2c485fef2e8cd4de863
Fil Zembowicz <fil@filosophy.org>
2019-07-09 11:21:11 -0400

    Revert "Add seat picker custom question as proof of concept"
    This reverts commit dd59859adb161bb307d7e6d066ff069533820330.
    
    Didn't mean to check into master repo

--- src/components/SeatPickerQuestion.scss.d.ts
+++
@@ -1,7 +0,0 @@
-export const app: string;
-export const container: string;
-export const plane: string;
-export const aisle: string;
-export const seat: string;
-export const unavailable: string;
-export const selected: string;
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
 import { render } from 'react-dom';
-import SeatPickerQuestion from './components/SeatPickerQuestion';
+import App from './components/App';
 
-render(<SeatPickerQuestion />, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
