5fec925162c686cd96900f5b175d3528eb849741
Fil Zembowicz <fil@formsort.com>
2020-03-23 09:21:12 -0400

    Make <App> a function instead of a class

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,12 @@
 import * as React from 'react';
 import * as css from './App.scss';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className={css.app}>
-        <h1>Hello, world!</h1>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className={css.app}>
+      <h1>Hello, world!</h1>
+    </div>
+  );
+};
 
 export default App;
